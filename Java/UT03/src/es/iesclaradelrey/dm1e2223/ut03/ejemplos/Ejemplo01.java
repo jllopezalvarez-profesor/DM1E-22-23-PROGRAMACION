f0333716e0d1cd15ba7e56ed71b7103ba46d4437
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2022-09-30 19:15:24 +0200

    Primer ejemplo java 20220930

---
+++ Java/UT03/src/es/iesclaradelrey/dm1e2223/ut03/ejemplos/Ejemplo01.java
@@ -0,0 +1,10 @@
+package es.iesclaradelrey.dm1e2223.ut03.ejemplos;
+
+public class Ejemplo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/UT03/src/es/iesclaradelrey/dm1e2223/ut03/ejercicios/Ejemplo01.java
@@ -0,0 +1,31 @@
+package es.iesclaradelrey.dm1e2223.ut03.ejercicios;
+
+public class Ejemplo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char c = 'e';
+		//char x = "e";
+		
+		
+		System.out.println("Hola");
+		
+		System.out.println("José Luis...");
+		
+		int año = 3;
+		
+		System.out.println("Cadena");
+		System.out.println('E');
+		System.out.println("E");
+		System.out.println(456);
+		System.out.print(23);
+		System.out.println(456.34);
+		System.out.println(año);
+		
+		
+		
+
+	}
+
+}
