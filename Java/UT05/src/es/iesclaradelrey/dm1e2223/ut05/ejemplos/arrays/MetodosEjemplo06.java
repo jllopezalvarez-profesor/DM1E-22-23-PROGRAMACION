cc8fabb75e916c9d754ea13250fcf858ea954f95
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2022-11-03 18:03:51 +0100

    Ejemplos arrays

---
+++ Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejemplos/arrays/MetodosEjemplo06.java
@@ -0,0 +1,15 @@
+package es.iesclaradelrey.dm1e2223.ut05.ejemplos.arrays;
+
+/**
+ * 
+ * Clase con métodos que vamos a usar en el ejemplo 06
+ *
+ */
+public class MetodosEjemplo06 {
+	public void muestraContenidoArray(int[] array) {
+		for (int numero : array) {
+			System.out.printf("%d ", numero);
+		}
+		System.out.println();
+	}
+}
