97c1ad16b61f894d8f021e8b3d48ccce63226c50
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2022-11-04 19:18:38 +0100

    Ejercicios

--- Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejemplos/arrays/Ejemplo06ParametroMetodo.java
+++ Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejemplos/arrays/Ejemplo06ParametroMetodo.java
@@ -18,10 +18,32 @@ public class Ejemplo06ParametroMetodo {
 		// Creación de un array e inicialización en una sóla línea
 		int[] array2 = { 15, 98, 67 };
 
-		// Creamos un objeto Metodos para poder llamar a métodos.
-		MetodosEjemplo06 metodos = new  MetodosEjemplo06();
-		metodos.muestraContenidoArray(array1);
-		metodos.muestraContenidoArray(array2);
-		
+		// Llamamos a los métodos estáticos para mostrar el array
+		muestraContenidoArray(array1);
+		muestraContenidoArray(array2);
+
 	}
+
+	/**
+	 * Muestra el contenido de un array escribiendo los valores seoparados por
+	 * espacio
+	 * 
+	 * @param array Array de números que queremos mostrar
+	 */
+
+	public static void muestraContenidoArray(int[] array) {
+		for (int numero : array) {
+			System.out.printf("%d ", numero);
+		}
+		System.out.println();
+	}
+
+	public static int[] generaArrayNumerosAleatorios(int cantidadNumeros) {
+		int[] numeros = new int[cantidadNumeros];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = ((int) (Math.random() * 100)) + 1;
+		}
+		return numeros;
+	}
+
 }
---
+++ Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejercicios/Ejercicio03.java
@@ -0,0 +1,49 @@
+package es.iesclaradelrey.dm1e2223.ut05.ejercicios;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Escribe un programa que: • Pregunte cuantos números vamos a procesar. • Cree
+ * un array del tamaño especificado. • Pida al usuario que introduzca los
+ * números uno a uno, y los vaya almacenando en el array. • Tome los números que
+ * el usuario ha introducido y los muestre en la consola. Primero desde el
+ * primero al último (el mismo orden en que los introdujo el usuario) y luego
+ * desde el último al primero.
+ *
+ */
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("¿Cuántos nombres vas a procesar?");
+		int cantidadNumeros = Integer.parseInt(scanner.nextLine());
+		// Si se usa nextInt, para que no falle (lea cadena vacía) la lectura de la
+		// primera cadena, tendríamos que hacer:
+		// - int cantidadNumeros = scanner.nextInt();
+		// - scanner.nextLine() // Esto elimina el salto de línea que nextInt no consume
+
+		String[] nombres = new String[cantidadNumeros];
+
+		for (int i = 0; i < nombres.length; i++) {
+			System.out.printf("Dime el nombre para la posición %d: ", i);
+			nombres[i] = scanner.nextLine();
+		}
+
+		for (String nombre : nombres) {
+			System.out.printf("%s, ", nombre);
+		}
+		System.out.println("\n");
+
+		System.out.println();
+		for (int i = nombres.length - 1; i >= 0; i--) {
+			String nombre = nombres[i];
+			System.out.printf("%s, ", nombre);
+		}
+		System.out.println("\n");
+
+		scanner.close();
+	}
+
+}
