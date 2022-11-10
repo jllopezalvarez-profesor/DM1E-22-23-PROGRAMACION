554a612597438b8e29778f71d073ad13e4ebaeef
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2022-11-10 18:24:15 +0100

    Ejercicio 10

--- Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejercicios/Ejercicio10.java
+++ Java/UT05/src/es/iesclaradelrey/dm1e2223/ut05/ejercicios/Ejercicio10.java
@@ -19,10 +19,73 @@ package es.iesclaradelrey.dm1e2223.ut05.ejercicios;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
+		int[] numeros = generarNumerosAleatorios(20, 1, 20);
+		mostrarArrayNumeros(numeros);
+//		mostrarArrayNumeros2(numeros);
+//		mostrarArrayNumeros3(numeros);
+		cambiarPrimosPorCeros(numeros);
+		mostrarArrayNumeros(numeros);
+	}
 
-		for (int i = 0; i < 30; i++)
-			System.out.println(generarNumeroAleatorio(5, 10));
+	public static void cambiarPrimosPorCeros(int[] numeros) {
+		for (int i = 0; i < numeros.length; i++) {
+			if (esPrimo(numeros[i])) {
+				numeros[i] = 0;
+			}
+		}
 
+	}
+
+	public static void mostrarArrayNumeros(int[] numeros) {
+		boolean enPrimerElemento = true;
+		for (int numero : numeros) {
+			if (enPrimerElemento) {
+				System.out.printf("%d", numero);
+				enPrimerElemento = false;
+			} else {
+				System.out.printf(",%d", numero);
+			}
+		}
+		System.out.println();
+	}
+
+	public static void mostrarArrayNumeros2(int[] numeros) {
+		for (int i = 0; i < numeros.length; i++) {
+			if (i == 0) {
+				System.out.printf("%d", numeros[i]);
+			} else {
+				System.out.printf(",%d", numeros[i]);
+			}
+		}
+		System.out.println();
+	}
+
+	public static void mostrarArrayNumeros3(int[] numeros) {
+		for (int i = 0; i < numeros.length; i++) {
+			if (i < numeros.length - 1) {
+				System.out.printf("%d,", numeros[i]);
+			} else {
+				System.out.printf("%d", numeros[i]);
+			}
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Gebnera un array de números aleatorios del tamaño especificado. Cada número
+	 * está entre mínimo y máximo ambos incluidos.
+	 * 
+	 * @param tamanio cantidad de números que se quieren generar
+	 * @param minimo valor mínimo (inclusivo) de los números generados
+	 * @param maximo valor máximo (inclusivo) de los números generados
+	 * @return el array con los números generados
+	 */
+	public static int[] generarNumerosAleatorios(int tamanio, int minimo, int maximo) {
+		int[] numeros = new int[tamanio];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = generarNumeroAleatorio(minimo, maximo);
+		}
+		return numeros;
 	}
 
 	/**
@@ -34,8 +97,18 @@ public class Ejercicio10 {
 	 */
 	public static int generarNumeroAleatorio(int minimo, int maximo) {
 
-		return (int) (Math.random() * (maximo - minimo+ 1)) + minimo;
+		return (int) (Math.random() * (maximo - minimo + 1)) + minimo;
 
+	}
+
+	static boolean esPrimo(int numero) {
+		if (numero < 2)
+			return false;
+		for (int i = 2; i < numero; i++) {
+			if (numero % i == 0)
+				return false;
+		}
+		return true;
 	}
 
 }
